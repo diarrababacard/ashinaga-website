ededc1e19bbcc83b713bbc368eb66948541eb95f
diarrababacard <128254250+diarrababacard@users.noreply.github.com>
2026-04-23 03:01:01 +0100

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,10 @@
 'use client'
 
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
- */
-
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import config from '@/sanity.config'
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
@@ -17,7 +13,6 @@ export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
     structureTool({structure}),
